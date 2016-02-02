10ddc0dfc3afe96dd8cdc2a5517ccb0aa0829bbb
Jake Harris <milieu@javakat.io>
2016-02-02 17:12:03 -0600

    Remove as-of-yet-unnecessary error includes

--- src/event.js
+++ src/event.js
@@ -1,9 +1,6 @@
 'using strict';
 module.exports = Event
 
-var ParameterCountError = require('./errors').ParameterCountError,
-    NotImplementedError = require('./errors').NotImplementedError
-
 function Event () {
   
 }
