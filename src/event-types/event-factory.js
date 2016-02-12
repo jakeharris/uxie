0f76dda1361d6880579a86275940556e7da29c2b
Jake Harris <milieu@javakat.io>
2016-02-12 15:35:12 -0600

    Update of type names (maybe they should be dynamic) and addition of an EventFactory.generate() interface method

--- src/event-types/event-factory.js
+++ src/event-types/event-factory.js
@@ -5,12 +5,15 @@ var ParameterCountError = require('../errors').ParameterCountError,
     NotImplementedError = require('../errors').NotImplementedError
 
 function EventFactory(name, triggers) {
-  throw new NotImplementedError('This class is abstract by design. Extend this class (via proper use of prototypes -- see the DefaultEventTypesFactory for an example) if you want things to work out.')
+  throw new NotImplementedError('This class is abstract by design. Extend this class (via proper use of prototypes -- see TemporalEventFactory for an example) if you want things to work out.')
 }
 
 EventFactory.prototype.record = function () {
-  throw new NotImplementedError('This class is abstract by design. Extend this class (via proper use of prototypes -- see the DefaultEventTypesFactory for an example) if you want things to work out.')
+  throw new NotImplementedError('This class is abstract by design. Extend this class (via proper use of prototypes -- see TemporalEventFactory for an example) if you want things to work out.')
 }
 EventFactory.prototype.save = function () {
-  throw new NotImplementedError('This class is abstract by design. Extend this class (via proper use of prototypes -- see the DefaultEventTypesFactory for an example) if you want things to work out.')
+  throw new NotImplementedError('This class is abstract by design. Extend this class (via proper use of prototypes -- see TemporalEventFactory for an example) if you want things to work out.')
+}
+EventFactory.prototype.generate = function () {
+  throw new NotImplementedError('This class is abstract by design. Extend this class (via proper use of prototypes -- see TemporalEventFactory for an example) if you want things to work out.')
 }
