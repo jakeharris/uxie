24b0862f96bd4edcbc5649fbf01ef349a4efb87c
Jake Harris <milieu@javakat.io>
2016-03-24 16:49:47 -0500

    Remove param from outdated design

--- src/event-types/event-factory.js
+++ src/event-types/event-factory.js
@@ -3,7 +3,7 @@ module.exports = EventFactory
 var ParameterCountError = require('../errors').ParameterCountError,
     NotImplementedError = require('../errors').NotImplementedError
 
-function EventFactory(name, triggers) {
+function EventFactory(name) {
   'use strict';
   throw new NotImplementedError('This class is abstract by design. Extend this class (via proper use of prototypes -- see TemporalEventFactory for an example) if you want things to work out.')
 }
